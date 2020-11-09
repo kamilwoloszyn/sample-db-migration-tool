45804278536a3dcb5801844efc4d985d3c815165
Kamil <kamil.woloszyn1@gmail.com>
2020-11-09 10:03:41 +0100

    Changed returning value

--- api/db/move.go
+++ api/db/move.go
@@ -2,12 +2,12 @@ package db
 
 import (
 	"database/sql"
-	"errors"
 	"fmt"
 )
 
 func Merge(fromData []Schema, to *sql.DB) error {
-	if query, err := to.Prepare("INSERT INTO `customers` (`customerNumber`, `customerName`, `contactLastName`, `contactFirstName`, `phone`, `addressLine1`, `addressLine2`, `city`, `state`, `postalCode`, `country`, `salesRepEmployeeNumber`, `creditLimit`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"); err == nil {
+	query, err := to.Prepare("INSERT INTO `customers` (`customerNumber`, `customerName`, `contactLastName`, `contactFirstName`, `phone`, `addressLine1`, `addressLine2`, `city`, `state`, `postalCode`, `country`, `salesRepEmployeeNumber`, `creditLimit`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
+	if err == nil {
 		for _, item := range fromData {
 			if respond, errQuery := query.Exec(item.CustomerNumber, item.CustomerName, item.ContactLastName, item.ContactFirstName, item.Phone, item.AddressLine1, item.AddressLine2, item.City, item.State, item.PostalCode, item.Country, item.SalesRepEmployeeNumber, item.CreditLimit); errQuery == nil {
 				if id, errFetchID := respond.LastInsertId(); errFetchID == nil {
@@ -19,9 +19,8 @@ func Merge(fromData []Schema, to *sql.DB) error {
 				return errQuery
 			}
 		}
-	} else {
-		return err
 	}
-	return errors.New("please contact with technical support, undefinied behavior occured")
+
+	return err
 
 }
